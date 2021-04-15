7d9a21766fdfa297526018b92439e6086dca7323
SHUAKKI <kau@kaushiks-MacBook-Air.local>
2021-04-15 21:48:42 +0530

    Updates

---
+++ precache-manifest.95ce2762bc17d46f28e24e8687438e8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d1f98fd498888f22967f48227c6bfb3",
+    "url": "/harmony165/Quiz-App/index.html"
+  },
+  {
+    "revision": "1a8b0684d07c40ae5777",
+    "url": "/harmony165/Quiz-App/static/css/main.8c4feaf6.chunk.css"
+  },
+  {
+    "revision": "27407d1211b53a50fe42",
+    "url": "/harmony165/Quiz-App/static/js/2.bb638460.chunk.js"
+  },
+  {
+    "revision": "1a8b0684d07c40ae5777",
+    "url": "/harmony165/Quiz-App/static/js/main.ed27abe9.chunk.js"
+  },
+  {
+    "revision": "80ad3c6dce5ef0ba97a9",
+    "url": "/harmony165/Quiz-App/static/js/runtime-main.27a6cb76.js"
+  }
+]);
